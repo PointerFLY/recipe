95c34894c9f4042a7ce92b60cc93405886e55d1f
linghao.ma <linghao.ma@advancegroup.com>
2023-05-31 13:59:08 +0800

    feat: change to hex

--- siaolang/siaolang.py
+++ siaolang/siaolang.py
@@ -2,17 +2,15 @@ import base64
 
 
 def text2siao(text: str):
-    hex = text.encode("utf-8").hex().encode("ascii")
-    b64 = base64.b32encode(hex)
+    hex = text.encode('utf-8').hex()
+    hex_bytes = bytes(hex, encoding='ascii')
     ret = []
 
-    for c in b64:
-        if c == ord("="):
-            continue
-        if c >= ord("A"):
-            n_siao = c - ord("A") + 7
+    for c in hex_bytes:
+        if c >= ord('a'):
+            n_siao = c - ord('a') + 10
         else:
-            n_siao = c - ord("1")
+            n_siao = c - ord('0')
 
         n_20 = 0
         n_10 = 0
@@ -31,7 +29,7 @@ def text2siao(text: str):
             n_10 += 1
             siaos = siaos // 10
             remain = remain % 10
-
+            
         siaos = remain // 5
         remain = remain % 5
         while siaos:
@@ -40,52 +38,46 @@ def text2siao(text: str):
             remain = remain % 5
 
         for _ in range(remain):
-            ret.append("siao")
+            ret.append('siao')
         for _ in range(n_5):
-            ret.append("siaomeh")
+            ret.append('siaomeh')
         for _ in range(n_10):
-            ret.append("siaoleh")
+            ret.append('siaoleh')
         for _ in range(n_20):
-            ret.append("siaoalready")
-        ret.append(" ")
+            ret.append('siaoalready')
+        ret.append(' ')
 
     ret = ret[:-1]
-    return "".join(ret)
+    return ''.join(ret)
 
 
 def siao2text(siao_text: str):
-    words = siao_text.split(" ")
+    words = siao_text.split(' ')
     original_chars = []
     for word in words:
         n_siao = 0
         i = 0
 
         while i < len(word):
-            if word[i] == "s":
+            if word[i] == 's':
                 n_siao += 1
                 i += 4
-            elif word[i] == "m":
+            elif word[i] == 'm':
                 n_siao += 4
                 i += 3
-            elif word[i] == "l":
+            elif word[i] == 'l':
                 n_siao += 9
                 i += 3
-            elif word[i] == "a":
+            elif word[i] == 'a':
                 n_siao += 19
                 i += 7
             else:
-                raise ValueError("Illegal character")
-
-        if n_siao >= 7:
-            c = n_siao - 7 + ord("A")
+                raise ValueError('Illegal character')
+        
+        if n_siao > 10:
+            c = n_siao - 10 + ord('a')
         else:
-            c = n_siao + ord("1")
+            c = n_siao + ord('0')
         original_chars.append(c)
 
-    padding = 8 - len(original_chars) % 8
-    for _ in range(padding):
-        original_chars.append(ord("="))
-
-    return bytes.fromhex(
-        base64.b32decode(bytes(original_chars)).decode("ascii")
-    ).decode("utf-8")
+    return bytes.fromhex(bytes(original_chars).decode('ascii')).decode('utf-8')
